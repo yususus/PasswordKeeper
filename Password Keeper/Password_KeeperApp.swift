12dfad2e6f18767d0d83413e7ca0a68b93b7c7c2
Yusuf Aydın <yusayd3825@gmail.com>
2024-05-27 12:41:31 +0300

    created

---
+++ Password Keeper/Password_KeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Password_KeeperApp.swift
+//  Password Keeper
+//
+//  Created by yusuf on 27.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Password_KeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
