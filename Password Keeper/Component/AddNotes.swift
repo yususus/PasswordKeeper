a2b7a5d6aa61049f6c1f37ffe54e2f0783a2ce3d
Yusuf Aydın <yusayd3825@gmail.com>
2024-08-03 09:15:19 +0300

    completed add notes view

--- Password Keeper/Component/AddNotes.swift
+++ Password Keeper/Component/AddNotes.swift
@@ -8,12 +8,15 @@
 import SwiftUI
 
 struct AddNotes: View {
-    @State var text: String = ""
+    @State var title: String = ""
+    @State var noteText: String = ""
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
     var body: some View {
         VStack {
             ScrollView {
                 VStack(alignment: .leading) {
-                    TextField("Title", text: $text)
+                    TextField("Title", text: $title)
                         .font(.title)
                         .padding()
                     
@@ -25,25 +28,45 @@ struct AddNotes: View {
                     .overlay(.black)
                 VStack(alignment: .leading) {
                     VStack {
-                        TextField("Placeholder", text: $text, axis: .vertical)
+                        TextField("Placeholder", text: $noteText, axis: .vertical)
                             .padding()
                             .cornerRadius(15)
                             .padding()
                     }
                 }
             }
-        }
+        }.background(Color.brown.opacity(0.5))
+        
         //sanki başka bir sayfadan gelmiş gibi davranır
-        .toolbar {
-            ToolbarItem(placement: .navigationBarTrailing) {
-                Button(action: {
-                    // Butonun işlevi burada olacak
-                }, label: {
-                    Text("Save")
-                        .fontWeight(.bold)
-                })
+            .toolbar {
+                // Back butonunu özelleştirmek için
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: {
+                        // Geri gitme aksiyonu
+                        self.presentationMode.wrappedValue.dismiss()
+                        
+                    }) {
+                        HStack {
+                            Image(systemName: "arrowshape.left.fill")
+                                .foregroundStyle(Color.black)// Geri butonu simgesi
+                        }
+                    }
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        // Butonun işlevi burada olacak
+                    }, label: {
+                        Text("Save").foregroundStyle(Color.white)
+                            .frame(width: 100,height: 30)
+                            .background(Color.brown.opacity(0.8))
+                            .clipShape(.rect(cornerRadius: 10))
+                            .fontWeight(.bold)
+                    })
+                }
             }
-        }
+            .navigationBarBackButtonHidden(true)
+            .toolbarBackground(Color.brown.opacity(0.5), for: .navigationBar)
+            .toolbarBackground(.visible, for: .navigationBar)
     }
 }
 
